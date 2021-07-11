04c1b9f572df02052443cca27f25156039081d9d
prajaktadad <87257953+prajaktadad@users.noreply.github.com>
2021-07-11 22:30:27 +0530

    created hello4.c
    This is program to print fibonacci series without recursion

---
+++ hello4.c
@@ -0,0 +1,14 @@
+class fibonacciExample 1{
+  public static void main(string args[])
+  {
+    int n1=0,n2=1,n3,i,count=10;
+    system.out.print(n1+" "=n2);
+    
+    for(i=2; i<count; ++i)
+    {
+      n3=n1+n2;
+      system.out.print(""+n3);
+      n1=n2;
+      n2=n3;  
+    }
+  }   
