80859dda20f12f86426c4ecdb19dfc6608fb210d
prajaktadad <87257953+prajaktadad@users.noreply.github.com>
2021-07-18 13:03:09 +0530

    Created hello7.c
    This program is an example of switch statement to check whether that person is 18 ,19 or 20.

---
+++ hello7.c
@@ -0,0 +1,22 @@
+public static void main(string[] args)
+{
+  int user = 18;
+  switch( user ) {
+    case 18:
+       system.out.println("you're 18")
+         break;
+    case 19:       
+      system.out.println("you're 19")
+         break;
+    case 20:       
+      system.out.println("you're 20")
+         break; 
+    default:
+      system.out.println("you're not 18,19 or 20");
+         break;
+  }
+  
+}
+      
+      
+      
